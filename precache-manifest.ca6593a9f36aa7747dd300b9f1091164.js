28492fc41f139113891ef90e774d9b2e4138b0a5
MantasMikal <mikalauskas.it@gmail.com>
2019-11-21 21:35:43 +0000

    Updates

---
+++ precache-manifest.ca6593a9f36aa7747dd300b9f1091164.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca4622767f63c03f467115f02699a50",
+    "url": "/flamingo/index.html"
+  },
+  {
+    "revision": "6536bb50cbdd70364ffd",
+    "url": "/flamingo/static/css/main.9a2b8d45.chunk.css"
+  },
+  {
+    "revision": "04803634b93f636fd738",
+    "url": "/flamingo/static/js/2.bcfb04ec.chunk.js"
+  },
+  {
+    "revision": "6536bb50cbdd70364ffd",
+    "url": "/flamingo/static/js/main.d6e3e100.chunk.js"
+  },
+  {
+    "revision": "7c46d5695bb9e6df782f",
+    "url": "/flamingo/static/js/runtime-main.99aab15c.js"
+  },
+  {
+    "revision": "35408ec22ec834e3f160126500108e97",
+    "url": "/flamingo/static/media/babyFlamigo.35408ec2.svg"
+  },
+  {
+    "revision": "03e274c57bd914dd33719207b17cae02",
+    "url": "/flamingo/static/media/beachy.03e274c5.svg"
+  },
+  {
+    "revision": "edcb726afdcc72e39dad3f34f0eed7a9",
+    "url": "/flamingo/static/media/bossFlamingo.edcb726a.svg"
+  },
+  {
+    "revision": "28c19f8486d3f098cbf5b81f827f34d5",
+    "url": "/flamingo/static/media/chair.28c19f84.svg"
+  },
+  {
+    "revision": "73306f6f13f56dcc8b0ff39e95e314fa",
+    "url": "/flamingo/static/media/cloud1.73306f6f.svg"
+  },
+  {
+    "revision": "ea82c2cff6b8b7f2b7f1fb4209536d60",
+    "url": "/flamingo/static/media/cloud2.ea82c2cf.svg"
+  },
+  {
+    "revision": "825364e79c39fecfbe18b0376b62b50c",
+    "url": "/flamingo/static/media/cloud3.825364e7.svg"
+  },
+  {
+    "revision": "ed0a7eccf8a5f82e30366e13a347fe08",
+    "url": "/flamingo/static/media/egg.ed0a7ecc.svg"
+  },
+  {
+    "revision": "b292d46f5f7db07281b419b4c3b72e3a",
+    "url": "/flamingo/static/media/fire.b292d46f.svg"
+  },
+  {
+    "revision": "23632531b259c13319a52303a159e418",
+    "url": "/flamingo/static/media/flamingo.23632531.svg"
+  },
+  {
+    "revision": "18993ffa2731c3367f71fda9754b5b8e",
+    "url": "/flamingo/static/media/flyingFlamingo.18993ffa.svg"
+  },
+  {
+    "revision": "49d5357c5fb413eb9d1018a50b3e15e3",
+    "url": "/flamingo/static/media/greaterFlamingo.49d5357c.svg"
+  },
+  {
+    "revision": "a0938a9f1b2bf3c8d49543d199c9014d",
+    "url": "/flamingo/static/media/leftLeaf.a0938a9f.svg"
+  },
+  {
+    "revision": "ed18df8a7f34b4f678500317182cfcea",
+    "url": "/flamingo/static/media/leftLeafDown.ed18df8a.svg"
+  },
+  {
+    "revision": "9262aafc250ef834fac60511fe34ba2f",
+    "url": "/flamingo/static/media/leftUmbrella.9262aafc.svg"
+  },
+  {
+    "revision": "8f136c908b022de7c38a5eaf74ed2516",
+    "url": "/flamingo/static/media/leftUmbrella_2.8f136c90.svg"
+  },
+  {
+    "revision": "c122e5be194784b92a6682fd75993a09",
+    "url": "/flamingo/static/media/legs.c122e5be.svg"
+  },
+  {
+    "revision": "aa943dea0552d4a2b62e4566680e2fce",
+    "url": "/flamingo/static/media/life.aa943dea.svg"
+  },
+  {
+    "revision": "a2875e53ba9cedaef8ced3fb94e47f3b",
+    "url": "/flamingo/static/media/middleUmbrella.a2875e53.svg"
+  },
+  {
+    "revision": "7a6e002f11db9c83fea092a6ecdb2f7e",
+    "url": "/flamingo/static/media/neverAlone.7a6e002f.svg"
+  },
+  {
+    "revision": "0544c20a1ecd8958eaad40dc7c660a2a",
+    "url": "/flamingo/static/media/rightLeaf.0544c20a.svg"
+  },
+  {
+    "revision": "042d91217a5999783f1fdb9272c5b35f",
+    "url": "/flamingo/static/media/sand.042d9121.svg"
+  },
+  {
+    "revision": "f0f189b7dba6e3929c3b9d0bfd44099e",
+    "url": "/flamingo/static/media/shrimp.f0f189b7.svg"
+  }
+]);
